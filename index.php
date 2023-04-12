f8eee2139cbaa0ec30dab53de3999ff66561a92e
Nadim Mahmud <nadeemmaahmud@gmail.com>
2023-04-12 22:14:43 +0600

    Second Commit

--- index.php
+++ index.php
@@ -31,7 +31,6 @@
                     </div>
                 </div>
             </nav>
-            <!-- Header-->
             <header class="py-5">
                 <div class="container px-5 pb-5">
                     <div class="row gx-5 align-items-center">
